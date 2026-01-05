b5f30c5abe3fd819d48ef2822078a615a1a553f6
yinghau68 <yinghautsai68cycu@gmail.com>
2026-01-05 22:21:00 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter, BrowserRouter } from 'react-router-dom'
 import { ContextProvider } from './Context/Context.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <ContextProvider>
       <App />
     </ContextProvider>
 
-  </BrowserRouter >
+  </HashRouter >
 
 )
